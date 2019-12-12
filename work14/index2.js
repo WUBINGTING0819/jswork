479dddbf702ddd46a0f806d55799c04a37aea5b7
WUBINGTING0819 <970759622@qq.com>
2019-12-12 11:38:27 +0800

    work16

--- work14/index2.js
+++ work14/index2.js
@@ -1,16 +1,16 @@
 let time = new Date()
 let fenzhong = miao = 0;
-fenzhong = 60 - time.getMinutes()
-miao = 60 - time.getSeconds()
+fenzhong = 59 - time.getMinutes()
+miao = 59 - time.getSeconds()
 let id =setInterval(seckill,1000)
 function seckill() {
     miao--
     if(miao== -1){
-        miao= 60
+        miao= 59
         fenzhong--
     } 
     if(fenzhong == -1){
-        fenzhong = 60
+        fenzhong = 59
     } 
     document.getElementById('fenzhong').innerHTML = fenzhong +'分'
     document.getElementById('miao').innerHTML = miao +'秒'
